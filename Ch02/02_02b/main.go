8e183df5a5feacaffddb5626aa5d57877f1a9d2c
claudioabudio <47932948+claudioabudio@users.noreply.github.com>
2026-07-17 19:03:18 +0000

    initial  commit golang refresher

--- Ch02/02_02b/main.go
+++ Ch02/02_02b/main.go
@@ -6,9 +6,15 @@ import (
 
 func main() {
 
-	// str1 := "The quick red fox"
-	// str2 := "jumped over"
-	// str3 := "the lazy brown dog."
+	str1 := "The quick red fox"
+	str2 := "jumped over"
+	str3 := "the lazy brown dog."
+	aNumber := 42
 
-	fmt.Println("Hello from Go!")
+	fmt.Println(str1, str2, str3)
+	stringLen, err := fmt.Println(aNumber)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("number of bytes written:", stringLen)
 }
