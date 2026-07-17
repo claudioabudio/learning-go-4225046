cfa4a8276ac183dcbcf612e42d8821c196aa39e0
claudioabudio <47932948+claudioabudio@users.noreply.github.com>
2026-07-17 22:47:07 +0000

    reading input from os.Stdin, use of strconv and strings packages

--- Ch02/02_03b/main.go
+++ Ch02/02_03b/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
+func main() {
+	bufReader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter text: ")
+	inputText, _ := bufReader.ReadString('\n')
+	fmt.Println(inputText)
+	fmt.Println("Enter float number:")
+	inputText, _ = bufReader.ReadString('\n')
+	f, err := strconv.ParseFloat(strings.TrimSpace(inputText), 64)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("Number: %f\n", f)
+	}
 }
