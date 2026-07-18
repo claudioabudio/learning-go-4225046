b0aad88545d87b1d367b95bc2f49b4bd4273c109
claudioabudio <47932948+claudioabudio@users.noreply.github.com>
2026-07-18 17:57:54 +0000

    using math.Round

--- Ch02/02_04b/main.go
+++ Ch02/02_04b/main.go
@@ -4,4 +4,8 @@ import "fmt"
 
 func main() {
 	fmt.Println("Math")
+	i1, i2, i3 := 10, 20, 30 
+	fmt.Println("Sum of i1 + i2 + i3 =", i1+i2+i3)
+	f1, f2, f3 := 23.5, 65.1, 76.3
+	fmt.Println("Sum of f1+f2+f3=", f1+f2+f3)
 }
--- Ch02/02_05b/main.go
+++ Ch02/02_05b/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
@@ -9,5 +10,8 @@ func main() {
 	f1, f2, f3 := 23.5, 65.1, 76.3
 	sum := f1 + f2 + f3
 	fmt.Println("Float sum:", sum)
+	fmt.Printf("Float sum with 2 decimals: %.2f\n ", sum)
+	sumRounded := math.Round(sum * 100) / 100
+	fmt.Println("Float sum rounded: ", sumRounded)
 
 }
